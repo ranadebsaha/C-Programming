37cff75a1ddc939b0c4af40ecfa81da65dd67226
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2023-12-29 20:26:30 +0530

    Create Sum_of_nth_natural_number.c

---
+++ Sum_of_nth_natural_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+int i,a=0,n;
+
+printf("Enter last value of the range: ");
+
+scanf("%d",&n);
+
+for(i=0;i<=n;i++)
+
+a=a+i;
+
+printf("Sum of the all natural number between 1 to %d is: %d",n,a);
+}
