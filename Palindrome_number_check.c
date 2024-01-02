5de7b71d8f466c914b04a976016a030d299473ad
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-02 19:17:11 +0530

    Create Palindrome_number_check.c

---
+++ Palindrome_number_check.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//
+
+//int main() {
+
+//int a,b,i,c=0;
+
+//printf("Enter a number: ");
+
+//scanf("%d",&a);
+
+//b=a;
+
+//while(a>0)
+
+//{
+
+//    i=a%10;
+
+//    c=c*10+i;
+
+//    a=a/10;
+
+//}
+
+//if(b==c)
+
+//printf("This is a Palindrome Number.");
+
+//else
+
+//printf("This is not a Palindrome Number.");
+
+//}
