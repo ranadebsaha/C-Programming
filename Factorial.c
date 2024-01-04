b41e9f5096510fbee84678bb8891b66d931e0737
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-04 23:34:36 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//
+
+//int main() {
+
+//int a,b=1,i;
+
+//printf("Enter a number: ");
+
+//scanf("%d",&a);
+
+//for(i=1;i<=a;i++)
+
+//{
+
+//    b=b*i;
+
+//}
+
+//printf("Factorial number of '%d' this number is: %d",a,b);
+
+//}
+
+//
