04c44c9b0f89254d1253810c90319822d292b9fe
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-05 23:37:32 +0530

    Create Prime_number.c

---
+++ Prime_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//
+
+//int main() {
+
+//int n,b=1,i,j,a;
+
+//printf("Enter the last number of range: ");
+
+//scanf("%d",&n);
+
+//printf("The Prime numbers are: ");
+
+//for(i=1;i<=n;i++)
+
+//{
+
+//    a=0;
+
+//    for(j=1;j<=i;j++)
+
+//    {
+
+//        if(i%j==0)
+
+//          a++;
+
+//    } 
+
+//if(a==2)
+
+//printf("%d\t",i);
+
+//}
+
+//}
